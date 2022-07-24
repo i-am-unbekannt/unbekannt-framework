d93f8eec183b1c7ad3a6ed9c9b66d79b91cc84e9
Unbekannt <89654239+i-am-unbekannt@users.noreply.github.com>
2022-07-24 17:05:20 +0000

    Update install.py

--- install.py
+++ install.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-import os
+import os, sys
 
 if os.geteuid() != 0:
   exit("[*] ERROR: Permission denied\n[*] Please try again, using 'sudo python3 install.py'\n")
 
+print(""""
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+#   Unbekannt Framwork 6.0 Installer          #
+#                                             #
+#   Github:    github.com/i-am-unbekannt      #
+#   Instagram: instagram.com/i_am_unbekannt   #
+#   Flowcode:  flowcode.com/page/unbekannt    #
+#                                             #
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+""")
 
-print("[*] Installing mkd3...")
-os.system("sudo apt-get install mdk3")
+ans = input("[*] Start Installing? [y/n] ")
+if ans == "n":
+  sys.exit()
 
-print("[*] Installing macchanger...")
-os.system("sudo apt-get install macchanger")
+if ans == "y":
+  print("<================================>\n[*] Installing mkd3...\n<================================>")
+  os.system("sudo apt-get install mdk3")
 
-print("[*] Installing python requirements...")
-os.system("pip3 install -r requirements.txt")
+  print("<================================>\n[*] Installing macchanger...\n<================================>")
+  os.system("sudo apt-get install macchanger")
 
-print("[!] Done")
+  print("<================================>\n[*] Installing python requirements...\n<================================>")
+  os.system("pip3 install -r requirements.txt")
+
+  print("\n\n[*] Done!\n[*] Starting Unbekannt Framework...")
+  os.system("sudo python3 Unbekannt.py")
